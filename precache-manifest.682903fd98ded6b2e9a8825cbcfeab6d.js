67a9ef2ad2d447514b050428290207743715fcc3
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-03-15 20:01:28 +0200

    Updates

---
+++ precache-manifest.682903fd98ded6b2e9a8825cbcfeab6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99d003d9fe28136f4e43293a0bc628c9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "df171c63963d73b20efd",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "ee5b646a280bb4fb89cb",
+    "url": "./static/css/main.5b5eb71e.chunk.css"
+  },
+  {
+    "revision": "df171c63963d73b20efd",
+    "url": "./static/js/2.cd70d490.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.cd70d490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee5b646a280bb4fb89cb",
+    "url": "./static/js/main.f56a59cb.chunk.js"
+  },
+  {
+    "revision": "7859211cf5cc2a17445c",
+    "url": "./static/js/runtime-main.2d746ae6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.682903fd98ded6b2e9a8825cbcfeab6d.js"
 );
 
 self.addEventListener('message', (event) => {
